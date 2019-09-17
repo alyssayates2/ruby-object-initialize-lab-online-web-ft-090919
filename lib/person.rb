e8a01eb459f52dbaa035a22ac6bc3898bcc25141
Alyssa Yates <bosslys@icloud.com>
2019-09-17 19:29:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,15 @@
 class Person
-  def initialize
-    beyonce = Person.new("Beyonce")
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
   end
 end
+
+beyonce = Person.new("Beyonce")
